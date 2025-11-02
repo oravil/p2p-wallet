368c098a25f4e6b6f5b7dd7c911cd68fc6d8de3d
Oravil <59480892+oravil@users.noreply.github.com>
2025-11-02 08:30:31 +0000

    Edited Spark

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -4,18 +4,18 @@ import { useKV } from '@github/spark/hooks'
 type Theme = 'light' | 'dark'
 
 interface ThemeContextType {
-}
+
   setTheme: (theme: Theme) => void
   toggleTheme: () => void
 }
 
-const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+const ThemeContext = createContext<ThemeContextType | undefined> (undefined)
 
   return (
       {children}
 
 
-  const context = useContext(ThemeContext
+  const context = useContext(ThemeContext)
     throw new Error('useTheme must be used
   return context
 
